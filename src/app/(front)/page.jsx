470fb09bb77e04818185a971e92a8c5f40cd3077
prymorsk <prymorsk@yahoo.com>
2026-01-08 11:11:53 +0530

    new

--- src/app/(front)/page.jsx
+++ src/app/(front)/page.jsx
@@ -86,7 +86,7 @@ const [
       bannerContent={pageMeta?.data.home}
       categories={categories}
       states={states}
-      contractors={sitesetting.contractors}
+      contractors={sitesetting?.contractors}
     />
     </Suspense>
   );
